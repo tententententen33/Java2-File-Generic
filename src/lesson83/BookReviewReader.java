246c4dc2b0f5d0662757bfcae6ce1f7a310bc218
ten <bsi25017@uhe.ac.jp>
2026-01-05 11:51:58 +0900

    init

--- src/lesson83/BookReviewReader.java
+++ src/lesson83/BookReviewReader.java
@@ -1,14 +1,35 @@
 package lesson83;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class BookReviewReader {
 
 	public static void main(String[] args) {
-		ArrayList<__________> books = new ArrayList<>();
-		
-		
+		ArrayList<BookReview> books = new ArrayList<>();
+		try(BufferedReader br = new BufferedReader(new FileReader("booklist.txt"))) {
 			
+			String line;
+			while((line = br.readLine()) != null) {
+				if(!line.contains("：") || line.contains("登録冊数")) {
+					continue;
+				}
+			
+				String[] data = line.split("：");
+				String title = data[0];
+				String review = data[1];
+				
+				BookReview bookReview = new BookReview(title, review);
+				books.add(bookReview);
+			}
+			
+		}catch(IOException e){
+			System.out.println("ファイル読み込みエラー");
+		}
+		
+		
 
 		
 		for (BookReview book : books) {
